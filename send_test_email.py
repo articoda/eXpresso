87440601fbf000b6435bc392e761adffd56f7d83
articoda <daniele.artico@hu-berlin.de>
2025-10-07 22:22:49 +0200

    test arXiv email

--- send_test_email.py
+++ send_test_email.py
@@ -14,7 +14,7 @@ TO_ADDRESS = os.getenv('EMAIL_TO')
 
 # Create a text/plain message
 msg = EmailMessage()
-msg['Subject'] = 'Test Email Number 2 from GitHub Actions'
+msg['Subject'] = 'eXpresso0.2'
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = TO_ADDRESS
 msg.set_content('{}\n{}'.format(sys.argv[1],sys.argv[2]))
