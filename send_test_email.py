7b949032b6ae488363dded093addf12efcd141b6
articoda <daniele.artico@hu-berlin.de>
2025-10-07 22:02:00 +0200

    test arXiv email

--- send_test_email.py
+++ send_test_email.py
@@ -3,12 +3,9 @@
 import smtplib
 from email.message import EmailMessage
 import os
+import sys
 
-# Open the plain text file whose name is in textfile for reading.
-with open('body.txt') as fp:
-    # Create a text/plain message
-    msg = EmailMessage()
-    msg.set_content(fp.read())
+
 
 
 EMAIL_ADDRESS = os.getenv('EMAIL_USER')
@@ -18,6 +15,10 @@ TO_ADDRESS = os.getenv('EMAIL_TO')
 msg['Subject'] = 'Test Email Number 2 from GitHub Actions'
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = TO_ADDRESS
+# Create a text/plain message
+msg = EmailMessage()
+msg.set_content('{}\n{}'.format(sys.argv[1],sys.argv[2]))
+
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
